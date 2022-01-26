871486c59a2e8e36609052cf9553f9132baf189a
mrdedpl <mrdedpl@wp.pl>
2022-01-26 14:46:30 +0100

    Added display data annotation and repaired showing enum list (format) in create.cshtml from Books views.

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,7 +17,9 @@ namespace LibraryZPO.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        [Display(Name = "Published At")]
         public DateTime PublishedAt { get; set; }
+        [Display(Name = "Price")]
         public decimal RetailPrice { get; set; }
         public int Pages { get; set; }
         public Format Format { get; set; }
